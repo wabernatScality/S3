d57a03fd5227ee582bcf97fbc6ee494bcdb08277
Akim Sadaoui <akim.sadaoui@scality.com>
2018-01-25 16:50:37 +0100

    balance ton port

--- toto.sh
+++ toto.sh
@@ -4,6 +4,16 @@ set +x
 
 npm-run-all start_dmd start_s3server > npm-out 2> npm-err &
 while true
+do nc -w 1 localhost 9991
+    ret=$?
+    echo "ret: $ret"
+    if [ "$ret" -eq 0 ]
+    then
+        break
+    fi
+    sleep 0.5
+done
+while true
 do nc -w 1 localhost 9990
     ret=$?
     echo "ret: $ret"
