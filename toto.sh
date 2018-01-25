7d031cd2f6ac58a3b70ae83036ec9dea3225f9a7
Akim Sadaoui <akim.sadaoui@scality.com>
2018-01-25 17:19:28 +0100

    foo

--- toto.sh
+++ toto.sh
@@ -32,4 +32,4 @@ do nc -w 1 localhost 9990
     fi
     sleep 0.5
 done
-CI=true S3BACKEND=mem S3DATA=multiple mocha -t 20000 --no-exit --trace --recursive tests/multipleBackend
+CI=true S3BACKEND=mem S3DATA=multiple mocha -t 20000 --no-exit --recursive tests/multipleBackend
