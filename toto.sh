23623ddd3f54c1886308a4db0da32b3e1da8541c
Akim Sadaoui <akim.sadaoui@scality.com>
2018-01-25 17:18:05 +0100

    foo

--- toto.sh
+++ toto.sh
@@ -2,6 +2,15 @@
 
 set +x
 
+unset SECRET_AZURE_BACKEND_ACCESS_KEY
+unset SECRET_AZURE_BACKEND_ACCESS_KEY_2
+unset SECRET_AZURE_BACKEND_ACCOUNT_NAME
+unset SECRET_AZURE_BACKEND_ACCOUNT_NAME_2
+unset SECRET_AZURE_BACKEND_ENDPOINT
+unset SECRET_AZURE_BACKEND_ENDPOINT_2
+unset SECRET_AZURE_STORAGE_ACCOUNT
+unset SECRET_AZURE_STORAGE_ACCESS_KEY
+
 npm-run-all --parallel start_mdserver start_dataserver > npm-out 2> npm-err &
 while true
 do nc -w 1 localhost 9991
